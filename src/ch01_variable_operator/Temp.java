5cae28625283a267808e1aff36ae407fec33e63f
bluesky <blueksy@naver.com>
2025-07-23 15:22:03 +0900

    java commit

---
+++ src/ch01_variable_operator/Temp.java
@@ -0,0 +1,11 @@
+package ch01_variable_operator;
+
+public class Temp {
+    public static void main(String[] args) {
+        String name ;
+
+        name = "홍길동";
+
+        System.out.println("이름 : " + name);
+    }
+}
