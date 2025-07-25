18a3f7903a62d9d2ca0553ab176ef294e542d69b
bluesky <blueksy@naver.com>
2025-07-25 16:41:17 +0900

    sample test

---
+++ src/ch02_control_statement/MultiIf02.java
@@ -0,0 +1,28 @@
+package ch02_control_statement;
+
+public class MultiIf02 {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+파일 : MultiIf02.java
+
+int age = 10 ;
+double ticket = 1000.0 ;
+double discount ; // 할인율
+
+8세 미만 : "유아(무료 입장)"
+14세 미만 : "어린이(50% 할인)"
+20세 미만 : "청소년(30% 할인)"
+65세 이상 : "노인(40% 할인)"
+이외 : "성인(정가)"
+
+출력 예시 01:
+	10살 : 어린이(50% 할인)
+	가격은 500원입니다.
+
+출력 예시 01:
+	16살 : 청소년(30% 할인)
+	가격은 700원입니다.
+*/
